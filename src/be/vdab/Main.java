9d822c7e757951a15dc5b1d6c189c6b8f0053a2f
Arne <a.vaneycken@gmail.com>
2021-03-04 08:35:47 +0100

    Opgekuisd

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -16,17 +16,17 @@ public class Main {
 
         System.out.println("-5! = "+Recursive.faculteit(-5));
 
-        System.out.println("7! = "+Recursive.faculteit(7));
+        System.out.println("7! = "+Recursive.faculteit(7) + "\n");
 
-        System.out.println("4e fib cijfer: "+Recursive.fib(4));
+        System.out.println("4e fib cijfer: "+Recursive.fib(4) + "\n");
 
         System.out.println("-2^6 = "+Recursive.macht(-2.0,6));
 
         System.out.println("-2^7 = "+Recursive.macht(-2.0,7));
 
-        System.out.println("2^-3 = "+Recursive.macht(2.0,-3));
+        System.out.println("2^-3 = "+Recursive.macht(2.0,-3) + "\n");
 
-        System.out.println("GGD van 125.25 en 6.25: "+Recursive.ggd(125.25,65.25));
+        System.out.println("GGD van 125.25 en 6.25: "+Recursive.ggd(125.25,65.25) + "\n");
 
         var integerList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 
